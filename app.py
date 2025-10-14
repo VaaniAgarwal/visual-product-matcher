5170e55c28855fed8f9187bed86e1705d78436c5
Vaani Agarwal <vaaniagarwal0307@gmail.com>
2025-10-14 23:57:45 +0530

    Update app.py for dynamic port and optimize requirements

--- app.py
+++ app.py
@@ -96,5 +96,7 @@ def match_image():
 
     return render_template('match_form.html')
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
