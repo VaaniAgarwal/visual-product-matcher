8fd66acd28eda25ccb35cf00e20264561f05268f
Vaani Agarwal <vaaniagarwal0307@gmail.com>
2025-10-16 23:24:56 +0530

    Made some changes in app.py

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def match_image():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
