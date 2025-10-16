425c238c4279398ce421dcba62458819af3090cf
Vaani Agarwal <vaaniagarwal0307@gmail.com>
2025-10-16 23:26:44 +0530

    Made some changes in config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
         "DATABASE_URL",
         "postgresql://postgres:postgres@localhost:5432/visual_matcher_db"
     )
-    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
         
     SQLALCHEMY_TRACK_MODIFICATIONS = False
